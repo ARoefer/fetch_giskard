f4c28ef0a6b4efeece003bea2292a794c0327073
Adrian Röfer <aroefer@uni-bremen.de>
2017-10-02 17:44:40 -0400

    MOTOR_CONTROL: Added first version of a joint velocity controller for fetch's controller manager

---
+++ include/fetch_giskard/velocity_controller.h
@@ -0,0 +1,102 @@
+#pragma once
+
+#include <ros/ros.h>
+#include <robot_controllers_interface/controller.h>
+#include <robot_controllers_interface/joint_handle.h>
+#include <sensor_msgs/JointState.h>
+
+#include <unordered_map>
+#include <vector>
+#include <string>
+
+namespace robot_controllers {
+
+/**
+ * @brief Base class for a controller. Is derived from a Handle, so that
+ *        controllers can be passed from ControllerManager::getHandle(),
+ *        thus allowing controllers to access other controllers (to stack
+ *        their commands.
+ */
+class VelocityController : public Controller {
+  struct ControlledJoint {
+    JointHandlePtr joint;
+    ros::Time lastCommandStamp;
+    double desiredVelocity;
+    double desiredEffort;
+  };
+
+public:
+  /**
+   * @brief Default constructor, does almost nothing, all setup is done in init().
+   */
+  VelocityController()
+  : watchdogPeriod(0.05) { }
+
+  /**
+   * @brief Initialize the controller by reading the names of the controlled joints from the parameter server.
+   * @param nh Node handle for this controller.
+   * @param manager The controller manager instance, this is needed for the
+   *        controller to get information about joints, etc.
+   * @returns 0 if succesfully configured, negative values are error codes.
+   */
+  virtual int init(ros::NodeHandle& nh, ControllerManager* manager);
+
+  /**
+   * @brief Attempt to start the controller by subscribing to the "joint_velocity_commands" topic.
+   * @returns Always true.
+   */
+  virtual bool start();
+
+  /**
+   * @brief Attempt to stop the controller. Stops all joint movements.
+   * @param force Should we force the controller to stop? Some controllers
+   *        may wish to continue running until they absolutely have to stop.
+   * @returns Always true;
+   */
+  virtual bool stop(bool force);
+
+  /**
+   * @brief Cleanly reset the controller to it's initial state. Sets the desired velocity for all joints to zero and stops their motions.
+   * @returns True if successfully reset, false otherwise.
+   */
+  virtual bool reset();
+
+  /**
+   * @brief This is the update loop for the controller. It applies the current set of commands to the joints, as long as they're not older than the watchdog-period.
+   * @param time The system time.
+   * @param dt The timestep since last call to update.
+   */
+  virtual void update(const ros::Time& time, const ros::Duration& dt);
+
+  /** @brief Get the type of this controller. */
+  virtual std::string getType() {
+    return "JointVelocityController";
+  }
+
+  /** @brief Get the names of joints/controllers which this controller commands. */
+  virtual std::vector<std::string> getCommandedNames();
+
+  /** @brief Get the names of joints/controllers which this controller exclusively claims. */
+  virtual std::vector<std::string> getClaimedNames() { return std::vector<std::string>(); }
+
+  /** @brief Callback for refreshing joint commands. The stamp of the command needs to be set correctly */
+  virtual void commandCallback(const sensor_msgs::JointState& command);
+
+private:
+  /** @brief Internal function for stopping a joint */
+  void stopJoint(ControlledJoint& c);
+
+  /** @brief Watchdog period. If a joint's command is older than this period, the joint is stopped */
+  double watchdogPeriod;
+
+  /** @brief Subscriber for commands */
+  ros::Subscriber cmdSubscriber;
+
+  /** @brief Node handle of manager */
+  ros::NodeHandle nh;
+
+  /** @brief Map of joints controlled by this node */
+  std::unordered_map<std::string, ControlledJoint> controlledJoints;
+};
+
+}  // namespace robot_controllers
---
+++ src/velocity_controller.cpp
@@ -0,0 +1,121 @@
+#include <fetch_giskard/velocity_controller.h>
+
+#include <robot_controllers_interface/controller_manager.h>
+
+#include <sstream>
+#include <pluginlib/class_list_macros.h>
+
+PLUGINLIB_EXPORT_CLASS(robot_controllers::VelocityController, robot_controllers::Controller)
+
+namespace robot_controllers {
+
+int VelocityController::init(ros::NodeHandle& nh, ControllerManager* manager) {
+  Controller::init(nh, manager);
+
+  nh.getParam("watchdog_period", watchdogPeriod);
+
+  std::string paramCJ;
+  nh.getParam("controlled_joints", paramCJ);
+  std::stringstream jointStream(paramCJ);
+  while (!jointStream.eof()) {
+    std::string jname;
+    jointStream >> jname;
+    JointHandlePtr joint = manager->getJointHandle(jname);
+    if (!joint) {
+      ROS_ERROR_NAMED("JointVelocityController", "Can't get handle for joint '%s' from manager.", jname.c_str());
+      return -1;
+    }
+
+    // Effort is constant for now
+    controlledJoints[jname] = {joint, ros::Time(0), 0.0, joint->getEffortMax() * 0.75};
+  }
+
+  this->nh = nh;
+
+  return 0;
+}
+
+bool VelocityController::start() {
+  cmdSubscriber = nh.subscribe("joint_velocity_commands", 10, &VelocityController::commandCallback, this);
+  return true;
+}
+
+bool VelocityController::stop(bool force) {
+  for (auto it = controlledJoints.begin(); it != controlledJoints.end(); it++) {
+    it->second.joint->setVelocity(0, it->second.desiredEffort);
+    it->second.desiredVelocity = 0;
+  }
+  return true;
+}
+
+bool VelocityController::reset() {
+  for (auto it = controlledJoints.begin(); it != controlledJoints.end(); it++)
+    stopJoint(it->second);
+}
+
+void VelocityController::update(const ros::Time& time, const ros::Duration& dt) {
+  for (auto it = controlledJoints.begin(); it != controlledJoints.end(); it++) {
+    if ((it->second.lastCommandStamp - time).toSec() <= watchdogPeriod) {
+
+      double timeToLimit = 1;
+      if (it->second.desiredVelocity < 0) {
+        timeToLimit = (it->second.joint->getPositionMin() - it->second.joint->getPosition()) / it->second.desiredVelocity;
+      } else if (it->second.desiredVelocity > 0) {
+        timeToLimit = (it->second.joint->getPositionMax() - it->second.joint->getPosition()) / it->second.desiredVelocity;
+      }
+
+      if (timeToLimit < dt.toSec()) {
+        it->second.joint->setVelocity(it->second.desiredVelocity, it->second.desiredEffort);
+      } else {
+        stopJoint(it->second);
+      }
+    } else {
+      stopJoint(it->second);
+    }
+  }
+}
+
+std::vector<std::string> VelocityController::getCommandedNames() {
+  std::vector<std::string> out;
+  for (auto it = controlledJoints.begin(); it != controlledJoints.end(); it++)
+    out.push_back(it->first);
+  return out;
+}
+
+void VelocityController::commandCallback(const sensor_msgs::JointState& command) {
+  if (command.velocity.size() != command.name.size()) {
+    ROS_ERROR_NAMED("JointVelocityController", "Malformed command message. Velocity and name list must be of equal length.");
+    return;
+  }
+
+  // if (command.effort.size() != 0 && command.effort.size() != command.name.size()) {
+  //   ROS_ERROR_NAMED("JointVelocityController", "Malformed command message. Effort list must either be empty or have a number of entries matching the length of the name list.");
+  //   return;
+  // }
+
+  for (size_t i = 0; i < command.name.size(); i++) {
+    const std::string& name = command.name[i];
+    auto it = controlledJoints.find(name);
+
+    if (it != controlledJoints.end()) {
+      // Only consider commands that are newer
+      if (it->second.lastCommandStamp < command.header.stamp) {
+        double newVelocity = command.velocity[i];
+        if (abs(newVelocity) > it->second.joint->getVelocityMax())
+          newVelocity = copysign(newVelocity, it->second.joint->getVelocityMax());
+
+        it->second.desiredVelocity = newVelocity;
+        it->second.lastCommandStamp = command.header.stamp;
+      }
+    } else {
+      ROS_ERROR_NAMED("JointVelocityController", "Can't process command for joint '%s' as it's not commanded by this controller.", command.name[i].c_str());
+    }
+  }
+}
+
+void VelocityController::stopJoint(ControlledJoint& c) {
+  c.desiredVelocity = 0;
+  c.joint->setVelocity(0, c.desiredEffort);
+}
+
+}  // namespace robot_controllers
