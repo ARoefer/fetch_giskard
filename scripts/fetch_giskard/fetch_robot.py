70aaf98fa8d30f5bff79c916b91b362e05f3c248
Adrian Röfer <aroefer@uni-bremen.de>
2018-01-29 20:10:36 -0500

    Updated robot description and things

--- scripts/fetch_giskard/fetch_robot.py
+++ scripts/fetch_giskard/fetch_robot.py
@@ -1,19 +1,31 @@
 import operator
-from giskardpy.robot import Robot, Joint
+from giskard_affordances.dl_reasoning import SymbolicData, DLBodyPosture
+from giskardpy.robot import Robot, Joint, Gripper
+from giskard_affordances.utils import JointState
 from collections import namedtuple
 import giskardpy.symengine_wrappers as spw
 
-Gripper = namedtuple('Gripper', ['name', 'pose', 'opening', 'height', 'max_opening'])
 
 class Fetch(Robot):
 	def __init__(self, urdf='fetch.urdf'):
 		super(Fetch, self).__init__()
-		self._joints['gripper_joint'] = Joint(spw.Symbol('gripper_joint'), 0.1, 0.0, 0.1, False)
+		gripper_joint = Joint(spw.Symbol('gripper_joint'), 0.1, 0.0, 0.1, False)
+		self._joints['gripper_joint'] = gripper_joint
 
-		self.load_from_urdf_path(urdf, 'base_link', ['gripper_link', 'head_camera_link'])
+		self.load_from_urdf_path(urdf, 'base_link', ['r_gripper_finger_link', 'l_gripper_finger_link', 'head_camera_link'])
 
-		for joint_name in self.joint_constraints:
+		r_finger_joint = self._joints['r_gripper_finger_joint']
+		l_finger_joint = self._joints['l_gripper_finger_joint']
+		self._joints.pop('r_gripper_finger_joint', None)
+		self._joints.pop('l_gripper_finger_joint', None)
+		self.frames['r_gripper_finger_link'] = self.frames['r_gripper_finger_link'].subs({r_finger_joint.symbol: 0.5 * gripper_joint.symbol})
+		self.frames['l_gripper_finger_link'] = self.frames['l_gripper_finger_link'].subs({l_finger_joint.symbol: 0.5 * gripper_joint.symbol})
+
+		for joint_name, joint in self.joint_constraints.items():
 			self.set_joint_weight(joint_name, 0.01)
+
+		self.state = SymbolicData({jn: JointState(joint.symbol, 0, 0) for jn, joint in self._joints.items()}, self.do_js_resolve, ['joint_state'])
+
 
 		self.set_joint_weight('torso_lift_joint', 0.05)
 
@@ -21,7 +33,8 @@ class Fetch(Robot):
 							   pose=self.frames['gripper_link'],
 							   opening=spw.Symbol('gripper_joint'),
 							   height=0.03,
-							   max_opening=0.1)
+							   max_opening=0.1,
+							   link_name='gripper_link')
 
 		self.eef     = self.gripper.pose
 		self.camera  = self.frames['head_camera_link']
@@ -37,7 +50,11 @@ class Fetch(Robot):
 					   self.gripper.pose.subs(js),
 					   js['gripper_joint'],
 					   self.gripper.height,
-					   self.gripper.max_opening)
+					   self.gripper.max_opening,
+					   self.gripper.link_name)
+
+	def do_js_resolve(self, joint_state):
+		return {jname: JointState(joint_state[jname].position, joint_state[jname].velocity, joint_state[jname].effort) for jname in self.state.data}
 
 	def __str__(self):
 		return 'Fetch\'s state:\n' + reduce(operator.add, map(lambda t: t[0] + ': {:.3f}\n'.format(t[1]), self.joint_state.iteritems()), '')
